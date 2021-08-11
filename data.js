06233104e6deaf900ffbbb9a9741b8efe2ab2521
Prakash Shekhar <prakashshkhr@gmail.com>
2021-08-11 07:05:25 -0400

    Add Substantial

--- data.js
+++ data.js
@@ -18,10 +18,27 @@ var DATA = {
             { name: "a son of the son", cover: "https://www.jazzmusicarchives.com/images/covers/uyama-hiroto-a-son-of-the-sun-20190201151343.jpg", year: "2008", url: "https://www.youtube.com/watch?v=nR36PG-pZI8&list=OLAK5uy_nYPgHHFaU81I_Ii7OkALp4NghlUQ3jJRY" },
             { name: "freedom of the son", cover: "https://www.jazzmusicarchives.com/images/covers/uyama-hiroto-freedom-of-the-son-20190201151505.jpg", year: "2014", url: "https://www.youtube.com/watch?v=UaMimdxnFXk&list=PLrChK0G-IBUhwQUjCAtqUP5LMF4DBB4NL" },
             { name: "freeform jazz", cover: "https://www.jazzmusicarchives.com/images/covers/uyama-hiroto-freeform-jazz-20161104083425.jpg", year: "2017", url: "https://www.youtube.com/watch?v=gQaU0CjQfR8&list=PL4NXUZspQ7Byp5vvxGJfF5tV370kL-mtu" },
-            { name: "Japanoia", cover: "https://m.media-amazon.com/images/I/717LNDwa+jL._SS500_.jpg", year: "2020", url: "https://www.youtube.com/watch?v=0U3NN3weex8&list=OLAK5uy_nFpJNAC-7L7cmOU2_a8aNpXAUiOGKIt6g"}
+            { name: "Japanoia", cover: "https://m.media-amazon.com/images/I/717LNDwa+jL._SS500_.jpg", year: "2020", url: "https://www.youtube.com/watch?v=0U3NN3weex8&list=OLAK5uy_nFpJNAC-7L7cmOU2_a8aNpXAUiOGKIt6g" }
         ],
         sources: [
             { name: "Last FM Wiki", url: "https://www.last.fm/music/Uyama%20Hiroto/+wiki" }
         ]
     },
+    "Substantial": {
+        name: "Substantial",
+        bio: "Prince George’s County, Maryland-born MC, producer, artist, educator, and author Substantial, debuted in 2000 collaborating with the late Japanese producer, Nujabes, in which he is featured on Metaphorical Music, Modal Soul, and Spiritual State. Legendary rapper and activist, Chuck D of Public Enemy referred to Substantial as 'One of the great MCs of our time.'",
+        discography: [
+            { name: "To This Union A Sun Was Born", cover: "https://f4.bcbits.com/img/a1060921883_16.jpg", year: "2001", url: "https://www.youtube.com/watch?v=f2dW-3eMh_A&list=OLAK5uy_kchsXCGCjWLEy3NOhkLC98Of27qTIh8hw&index=1",},
+            { name: "Substantial Evidence", cover: "https://f4.bcbits.com/img/a2923622150_16.jpg", year: "2003", url: "https://www.youtube.com/watch?v=urr60M16DDE&list=OLAK5uy_kXs-i6Lw-nYoMd_ck_pw7WIFIms8-nzt8&index=1",},
+            { name: "Sacrifice", cover: "https://f4.bcbits.com/img/a1006860033_16.jpg", year: "2008", url: "https://www.youtube.com/watch?v=Fr-P7Jo-kjc&list=OLAK5uy_lzOG9ezelM1c5Dc7U7deA86QOyk63yZck",},
+            { name: "Home Is Where the Art Is", cover: "https://f4.bcbits.com/img/a4084548831_16.jpg", year: "2012", url: "https://www.youtube.com/watch?v=51MCVUlOW0M&list=OLAK5uy_kNWLTzcN4_CQJlkdbWB3RUvKcoWim3XWE",},
+            { name: "The Past Is Always Present in the Future", cover: "https://f4.bcbits.com/img/a2825504615_16.jpg", year: "2017", url: "https://www.youtube.com/watch?v=BlBgHu9SlG8&list=OLAK5uy_mMbCzuBJzlPQvFa2C-VbrT3FYxvlgo-Xw&index=1",},
+            { name: "Seeds", cover: "https://f4.bcbits.com/img/a0604208087_16.jpg", year: "2017", url: "https://www.youtube.com/watch?v=fYP-pm2c6Ns",},
+            { name: "The Garden", cover: "https://f4.bcbits.com/img/a1009568704_16.jpg", year: "2018", url: "https://www.youtube.com/watch?v=hFutpIsk-zQ&list=OLAK5uy_lb0WmVHxfxhrgl0bi-KJUuWEDnTAI71LM",},
+            { name: "Recompositions", cover: "https://f4.bcbits.com/img/a3437146784_16.jpg", year: "2019", url: "https://www.youtube.com/watch?v=SHHothI6Bp4&list=PLR2wqA739XnSIndXbOuNNWsDGjBshSDi5",},
+        ],
+        sources: [
+            { name: "Substantial's personal website", url: "https://www.iamsubstantial.com/bio", url: "",}
+        ]
+    }
 }
